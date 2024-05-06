d5799cec04c03bd19e665def0bd7660656f7e134
William Wingate <wtwingate@gmail.com>
2024-05-06 01:23:32 -0400

    Implement a basic REPL on the command line

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	prompt := "Pokédex >>> "
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for fmt.Print(prompt); scanner.Scan(); fmt.Print(prompt) {
+		line := scanner.Text()
+		if len(line) == 0 {
+			break
+		}
+		if command, ok := cliCommandMap[line]; ok {
+			command.callback()
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cliCommandMap = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Display a help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the program",
+		callback:    commandExit,
+	},
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("No one can hear your cries for help...")
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println()
+	fmt.Println("There is no escape!")
+	fmt.Println()
+	return nil
 }
